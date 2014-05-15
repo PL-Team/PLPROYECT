b9ba0d5a06d5443b895424913f33741649a51c8a
Eduardo Jesús Díaz Dioniz <eddioniz@gmail.com>
2014-05-15 18:51:48 +0100

    Actualizacion de mejoras para la producción de fichero

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var result;
 $(document).ready(function() {
   $('#parse').click(function() {
     try {
@@ -7,11 +8,28 @@ $(document).ready(function() {
       out.className = "unhidden";
       
       // Import: Change: $('#input').val() -> source, i forget it, again 
-      var result = parser.parse(source);
+      result = parser.parse(source);
       $('#output').html(JSON.stringify(result,undefined,2));
     } catch (e) {
       $('#output').html('<div class="error"><pre>\n' + String(e) + '\n</pre></div>');
     }
+  });
+  $('#download').click(function() {
+  	
+  	var aux='';
+  	for(var j=0;j < result.block.stament.length ;j++){
+  		aux += result.block.stament[j].id + ' ';
+  		for(var i=0; i < result.block.stament[j].set.value.length  ;i++){
+  			  aux += result.block.stament[j].set.value[i].x;
+  			  aux += ',';
+  		 	 	aux += result.block.stament[j].set.value[i].y;
+  		 		aux += ' ';
+  		}
+  		aux += 'END ';
+  	}
+  	// + result.block.stament[0].set
+  	//alert(aux);
+   	window.location.href = 'service.php?f='+aux;
   });
 });
 
